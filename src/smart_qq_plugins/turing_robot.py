d7e53fd157d8469a5ba0a74d91c3964d5286813d
darkz <darkz@dianhun.cn>
2016-09-13 13:00:35 +0800

    修改requests请求为传递params方式

--- src/smart_qq_plugins/turing_robot.py
+++ src/smart_qq_plugins/turing_robot.py
@@ -7,8 +7,6 @@ from smart_qq_bot.signals import on_all_message
 # 使用前请先前往 http://www.tuling123.com/register/index.jhtml
 # 申请 API key 谢谢
 # 另外需要 requests 支持
-# by darkz
-# 2016-09-08 18:59:54
 # 修改成调用图灵官方接口
 url = 'http://www.tuling123.com/openapi/api'
 apikey = ''
@@ -21,12 +19,11 @@ def turing_robot(msg, bot):
     """
 
     querystring = {
-        "key": "",
+        "key": apikey,
         "info": msg.content,
-        "userid": ""
     }
-    request_url = ("%s?key=%s&info=%s")%(url, apikey, msg.content)
-    response = requests.request("GET", request_url)
+
+    response = requests.request("GET", params=querystring)
 
     response_json = response.json()
     bot.reply_msg(msg, response_json.get('text'))
